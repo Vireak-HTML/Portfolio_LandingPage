6fb6231e8528a27a156fc9a4f8164303d516f87d
Chanvireak <vireakchan9626@gmail.com>
2026-08-26 00:42:17 +0700

    refactor the skill into js dependant

--- main.js
+++ main.js
@@ -1,3 +1,87 @@
+const skilldata = [
+  {
+    name: "HTML",
+    image: "IMG/html.png",
+  },
+  {
+    name: "CSS",
+    image: "IMG/css.png",
+  },
+  {
+    name: "JavaScript",
+    image: "IMG/js.png",
+  },
+  {
+    name: "Bootstrap",
+    image: "IMG/bootstrap.png",
+  },
+  {
+    name: "ReactJS",
+    image: "IMG/react.png",
+  },
+  {
+    name: "PHP",
+    image: "IMG/php.png",
+  },
+  {
+    name: "Laravel",
+    image: "IMG/laravel.png",
+  },
+  {
+    name: "JAVA",
+    image: "IMG/java.png",
+  },
+  {
+    name: "Git",
+    image: "IMG/git.png",
+  },
+  {
+    name: "GitHub",
+    image: "IMG/github.png",
+  },
+  {
+    name: "MySQL",
+    image: "IMG/mysql.png",
+  },
+  {
+    name: "Linux",
+    image: "IMG/linux-platform.png",
+  },
+  {
+    name: "C#",
+    image: "IMG/csharp.png",
+  },
+  {
+    name: "C++",
+    image: "IMG/cpp.png",
+  },
+  {
+    name: "Photoshop",
+    image: "IMG/photoshop.png",
+  },
+  {
+    name: "Illustrator",
+    image: "IMG/illustrator.png",
+  },
+  {
+    name: "Canva",
+    image: "IMG/canva.jpe",
+  },
+];
+
+    const cartContainer = document.getElementById("cardContainer");
+    skilldata.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card skill-card";
+      card.style =
+        "display: grid; grid-template-columns: 1fr 2fr; align-items: center;";
+      card.innerHTML = `<img src="${item.image}" alt="${item.name}" class="m-auto" width="30px">
+      <p class="skill-detail fs-4 fw-bolder m-0" >${item.name}</p>
+      `;
+      cartContainer.append(card);
+    });
+
+
 document.querySelectorAll(".nav-link").forEach((element) => {
   element.addEventListener("click", function () {
     document.querySelectorAll(".nav-link").forEach((el) => {
